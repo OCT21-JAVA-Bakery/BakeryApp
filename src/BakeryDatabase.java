09f0c8ff39adebf8ab1fe7c6fb9cebadfadfbe91
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-05 10:29:23 -0500

    BakeryDatabase, SearchDatabase

--- src/BakeryDatabase.java
+++ src/BakeryDatabase.java
@@ -12,4 +12,21 @@ public class BakeryDatabase extends ArrayList<BakeryItems> {
     public BakeryDatabase(Collection<? extends BakeryItems> c) {
         super(c);
     }
+
+    public void displayText() {
+        // this refer to itself
+        System.out.print(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (BakeryItems bakeryItems : this) {
+            sb.append(bakeryItems);
+            sb.append("\n");
+
+        }
+
+        return sb.toString();
+    }
 }
