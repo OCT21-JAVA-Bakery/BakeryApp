dab2bd28c3e338e0437a245a94e234dc0c8ac0e6
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-05 12:43:50 -0500

    SearchDatabase

---
+++ src/SearchDatabase.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class SearchDatabase {
+
+    private BakeryDatabase bakeryDatabase;
+
+    public SearchDatabase() {
+    }
+
+    public SearchDatabase(BakeryDatabase bakeryDatabase) {
+        this.bakeryDatabase = bakeryDatabase;
+    }
+
+    public BakeryDatabase getBakeryDatabase() {
+        return bakeryDatabase;
+    }
+
+    public void setBakeryDatabase(BakeryDatabase bakeryDatabase) {
+        this.bakeryDatabase = bakeryDatabase;
+    }
+
+    public String userInput(String prompt) {
+
+        System.out.print(prompt);
+
+//        Scanner kb = new Scanner(System.in);
+//
+//        String returnValue = kb.nextLine();
+//
+//        return returnValue;
+
+        return (new Scanner(System.in)).nextLine();
+    }
+
+    public BakeryDatabase searchDietRestriction(String dietRestriction) {
+        BakeryDatabase returnResult = new BakeryDatabase();
+        if (bakeryDatabase == null) // if not set berkeryDatabase yet
+            return returnResult;
+
+
+
+        for (BakeryItems bakeryItems : bakeryDatabase) {
+
+            for (String eachDietRestriction : bakeryItems.getDietRestriction()) {
+
+
+                if (eachDietRestriction.equalsIgnoreCase(dietRestriction) ||
+                        eachDietRestriction.equalsIgnoreCase("free of "+dietRestriction)
+                ) {
+                    returnResult.add(bakeryItems);
+                    break;
+                }
+
+            }
+        }
+
+        return returnResult;
+    }
+}
